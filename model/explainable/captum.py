17f7b49f9b3ee14db323997cea2f57d435fea792
Daniel Borges <44danbor@gmail.com>
2026-05-09 14:57:53 -0300

    update: refactor explanation extraction in captum.py

--- model/explainable/captum.py
+++ model/explainable/captum.py
@@ -1,5 +1,5 @@
 '''
-Arquivo voltadopara a explicação dos outputs das redes treinadas e avaliadas nesse diretório
+Arquivo voltado para a explicação dos outputs das redes treinadas e avaliadas nesse diretório
 Há dois contextos em que será usado:
     1. Explicar outputs das entradas originais e F-RecPlot
     2. Explicar outputs das entradas RP + GAF + MTF
@@ -19,35 +19,33 @@ from model.utils import transform
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def explain_2D(weigths_path, backbone, num_classes, img_path, target_class, classes):
-    
-    # Carregar Modelo
-    model = carregar_modelo(backbone, num_classes=num_classes, path_weights=weigths_path)
-    model.to(DEVICE)
-    model.eval()
+def extrac_explanation_2D(model, img_path, target_class, classes):
+    '''
+    Roda a imgem na rede e com o IG
+    Retorna dados brutos para visualização ou manipulação
+    '''
 
     # Preparar Imagens
     img_pil = Image.open(img_path).convert('RGB')
     input_tensor = transform(img_pil).unsqueeze(0).to(DEVICE)
     input_tensor.requires_grad = True
 
-    print(torch)
-
     with torch.no_grad():
         output_bruto = model(input_tensor)
         probabilidades = F.softmax(output_bruto, dim=1)[0]
         confianca, classe_pred = torch.max(probabilidades, dim=0)
         
-        confianca_pct = confianca.item() * 100
-        classe_pred_idx = classe_pred.item()
-
-        classe_predita = classes[classe_pred_idx]
-        classe_real = classes[target_class]
+        info_pred = {
+            "confianca_pct": confianca.item() * 100,
+            "classe_pred": classes[classe_pred.item()],
+            "classe_real": classes[target_class],
+            "target_class_idx": target_class
+        }
         
 
     # Calcula atribuições com IG
     ig = IntegratedGradients(model)
-    attributions, delta = ig.attribute(input_tensor, target=target_class,
+    attributions, _ = ig.attribute(input_tensor, target=target_class,
                                        n_steps=50, return_convergence_delta=True)
     
 
@@ -65,29 +63,87 @@ def explain_2D(weigths_path, backbone, num_classes, img_path, target_class, clas
     img_fundo = std * img_tensor + mean
     img_fundo = np.clip(img_fundo, 0, 1)
 
-    # Vizualização
-    print(f'o modelo previu a classe {classe_predita} com {confianca_pct:.2f}% de confiança e o correto é {classe_real}')
+    return input_tensor, attr_np, img_fundo, info_pred
 
-    titulo_grafico = f"Alvo XAI: {classe_real} | Predição: {classe_predita} ({confianca_pct:.2f}%)"
+def calcula_fidelidade_delecao(model, input_tensor, attr_np, target_class, steps=10):
+    '''
+    Remove prpgrassivamente os pixels os pixels mais importantes para a predição
+    segundo o IG e mede a queda de confiança da rede
+    '''
 
-    fig, axis = viz.visualize_image_attr(
+    importancia_2d = np.sum(np.abs(attr_np), axis=2)
+    importancia_flat = importancia_2d.flatten()
+
+    porcentagens = np.linspace(0, 100, steps+1)
+    confianca_registradas = []
+
+    for p in porcentagens:
+        if p == 0:
+            threshold = np.inf
+        else:
+            threshold = np.percentile(importancia_2d, 100-p)
+
+        mask_2d = (importancia_2d < threshold).astype(np.float32)
+        mask_tensor = torch.tensor(mask_2d).unsqueeze(0).unsqueeze(0).to(DEVICE)
+
+        tensor_perturbado = input_tensor * mask_tensor
+
+        with torch.no_grad():
+            output = model(tensor_perturbado)
+            probs = F.softmax(output, dim=1)[0]
+
+            confianca_alvo = probs[target_class].item() * 100
+            confianca_registradas.append(confianca_alvo)
+
+    return porcentagens, confianca_registradas
+    
+def visualize_results(attr_np, img_fundo, info_pred, porcentagens, confiancas):
+    '''
+    Gera figura com 2 gráficos: O mapa de calor e as curvas de deleção
+    '''
+
+    fig = plt.figure(figsize=(12,5))
+
+    ax1 = fig.add_subplot(1,2,1)
+    titulo_heat = f"XAI: {info_pred['classe_real']} | Pred: {info_pred['classe_pred']} ({info_pred['confianca_pct']}%)"
+
+    viz.visualize_image_attr(
         attr_np,                     
         img_fundo,
         method="blended_heat_map",   
         sign="positive",             
         show_colorbar=True,
-        title=titulo_grafico,
-        use_pyplot=True
+        title=titulo_heat,
+        plt_fig_axis=(fig, ax1),
+        use_pyplot=False
     )
 
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.plot(porcentagens, confiancas, marker='o', color='red', linestyle='-')
+    ax2.set_title('Métricas de Delação (Fidelidade)', fontsize=12)
+    ax2.set_xlabel(r'% de pixels mais importantes removidos')
+    ax2.set_ylabel(f"Confiança na Classe '{info_pred['classe_real']}' (%)")
+    ax2.set_ylim(0, 105)
+    ax2.grid(True, linestyle='--', alpha=0.6)
+
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
 
     weigths_path = r"C:\Users\IFTM-ITB\Desktop\EnsembleFractal\models\42\mobilenet_originais.pth"
+    img_path = r"C:\Users\IFTM-ITB\Desktop\EnsembleFractal\datasets\dataset_displasia\teste\severe\originais\12.tif"
     backbone = 'mobilenet'
     num_classes = 2
-    img_path = r"C:\Users\IFTM-ITB\Desktop\EnsembleFractal\datasets\dataset_displasia\teste\severe\originais\12.tif"
     target_class = 1 #severe
     classes = ["healthy", "severe"]
 
-    explain_2D(weigths_path, backbone, num_classes, img_path, target_class, classes)
+    model = carregar_modelo(backbone, num_classes=2, path_weights=weigths_path)
+    model.to(DEVICE)
+    model.eval()
+
+    input_tensor, attr_np, img_fundo, info = extrac_explanation_2D(model, img_path, target_class, classes)
+
+    porcentagens, confiancas = calcula_fidelidade_delecao(model, input_tensor, attr_np, target_class, steps=10)
+
+    visualize_results(attr_np, img_fundo, info, porcentagens, confiancas)
