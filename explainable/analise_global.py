1f8e656312ee9b9a4c264bfbc9ffb7b56d3c7add
Daniel Borges <44danbor@gmail.com>
2026-05-09 23:09:38 -0300

    feature: update global frequency map generation and plotting functions to include output directory handling

--- explainable/analise_global.py
+++ explainable/analise_global.py
@@ -1,3 +1,7 @@
+'''
+Esse arquivo é responsável por gerar os Mapas de Frequência Global a partir das explicações individuais (2D) de cada imagem, para cada classe. Ele também gera um mapa de contraste entre as classes.
+'''
+
 import os
 import glob
 import torch
@@ -9,6 +13,9 @@ import seaborn as sns
 from .xai_core import extrac_explanation_2D
 from model.model import carregar_modelo
 from model.utils import DEVICE
+
+actual_dir = script_dir = os.path.dirname(os.path.abspath(__file__))
+save_dir = os.path.join(actual_dir, "plots")
 
 def generate_global_freq_map(model, lista_imgs, target_class, classes, top_percent=15):
     '''
@@ -51,12 +58,12 @@ def generate_global_freq_map(model, lista_imgs, target_class, classes, top_perce
 
     return mapa_frequencia, total_corretas
 
-def plotar_mapa_global(mapa_frequencia, nome_classe, total_corretas):
+def plotar_mapa_global(mapa_frequencia, nome_classe, total_corretas, OUT_DIR):
     
     plt.figure(figsize=(10,8))
     
     sns.heatmap(mapa_frequencia, cmap='inferno', vmin=0, vmax=np.max(mapa_frequencia),
-                cbar_kws={'label': 'Frequncia^de Importância (1.0 = 100%)'})
+                cbar_kws={'label': 'Frequência de Importância (1.0 = 100%)'})
     
     plt.title(f'Assinatura Global F-RecPlot (Percolação) - Classe: {nome_classe}\n'
               f'Baseado em {total_corretas} predições corretas', fontsize=14, pad=15)
@@ -68,10 +75,13 @@ def plotar_mapa_global(mapa_frequencia, nome_classe, total_corretas):
     # plt.gca().invert_yaxis() 
     
     plt.tight_layout()
-    plt.savefig(f"mapa_global_{nome_classe}.png", dpi=300)
+
+    save_path = os.path.join(save_dir, OUT_DIR, f"mapa_global_{nome_classe}.png")
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    plt.savefig(save_path, dpi=300)
     plt.show()
 
-def plotar_mapa_contraste(mapa_severe, mapa_healthy):
+def plotar_mapa_contraste(mapa_severe, mapa_healthy, OUT_DIR):
     # Subtrai um do outro
     mapa_diff = mapa_severe - mapa_healthy
     
@@ -89,17 +99,21 @@ def plotar_mapa_contraste(mapa_severe, mapa_healthy):
     plt.ylabel("Eixo Y (Tempo/Atraso t1)")
     
     plt.tight_layout()
-    plt.savefig("mapa_contraste.png", dpi=300)
+    
+    save_path = os.path.join(save_dir, OUT_DIR, "mapa_contraste.png")
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    plt.savefig(save_path, dpi=300)
     plt.show()
 
 if __name__ == '__main__':
 
     #--- Configurações
-    weigths_path = r"models\42\mobilenet_RP_perc.pth" # Esse aqui é do continuous
-    backbone = 'mobilenet'
-    target_class = 0 #  saudavel = 0, severo = 1
+    weigths_path = r"models\42\efficientnet_b0_RP_perc.pth" # Esse aqui é do continuous
+    backbone = 'efficientnet_b0'
+    # target_class = 0  saudavel = 0, severo = 1
     classes = ["healthy", "severe"]
     num_classes = 2
+    OUT_DIR = "Continuous"
 
 
     # Tomar cuidado com BIN / CONTINUOUS
@@ -114,14 +128,14 @@ if __name__ == '__main__':
     model.to(DEVICE)
     model.eval()
 
-    mapa_freq_saudavel, qtd_acertos = generate_global_freq_map(
+    mapa_freq_saudavel, qtd_acertos_healthy = generate_global_freq_map(
         model, lista_img_saudavel, target_class=0, classes=classes, top_percent=20
     )
 
-    mapa_freq_severo, qtd_acertos = generate_global_freq_map(
+    mapa_freq_severo, qtd_acertos_severe = generate_global_freq_map(
         model, lista_img_severo, target_class=1, classes=classes, top_percent=20
     )
 
-    plotar_mapa_global(mapa_freq_saudavel, "Healthy", qtd_acertos)
-    plotar_mapa_global(mapa_freq_severo, "Severe", qtd_acertos)
-    plotar_mapa_contraste(mapa_freq_severo, mapa_freq_saudavel)
+    plotar_mapa_global(mapa_freq_saudavel, "Healthy", qtd_acertos_healthy, OUT_DIR)
+    plotar_mapa_global(mapa_freq_severo, "Severe", qtd_acertos_severe, OUT_DIR)
+    plotar_mapa_contraste(mapa_freq_severo, mapa_freq_saudavel, OUT_DIR)
