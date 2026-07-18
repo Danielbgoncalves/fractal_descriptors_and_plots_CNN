f61426fda0ce5c9b4029b9852dcc1195018621fb
Daniel Borges Gonçalves <danielbgoncalves@ufu.br>
2026-07-18 10:27:23 -0300

    Muda o carregamento de modelo para controle de camdas congeladas

--- model/model.py
+++ model/model.py
@@ -89,61 +89,61 @@ from .utils import *
 #     return model
 
 #saida4
-# def criar_modelo(
-#     backbone: str,
-#     num_classes: int,
-#     pretrained='DEFAULT',
-#     dropout_rate=0.2,
-#     trainable_blocks=3
-# ):
-#     backbone = backbone.lower()
+def criar_modelo(
+    backbone: str,
+    num_classes: int,
+    pretrained='DEFAULT',
+    dropout_rate=0.2,
+    trainable_blocks=3
+):
+    backbone = backbone.lower()
 
-#     if backbone == "mobilenet":
-#         model = models.mobilenet_v2(weights=pretrained)
+    if backbone == "mobilenet":
+        model = models.mobilenet_v2(weights=pretrained)
 
-#         # Congela tudo
-#         for param in model.parameters():
-#             param.requires_grad = False
+        # Congela tudo
+        for param in model.parameters():
+            param.requires_grad = False
 
-#         # Descongela os últimos blocos
-#         for param in model.features[-trainable_blocks:].parameters():
-#             param.requires_grad = True
+        # Descongela os últimos blocos
+        for param in model.features[-trainable_blocks:].parameters():
+            param.requires_grad = True
 
-#         in_features = model.classifier[1].in_features
-#         model.classifier = nn.Sequential(
-#             nn.Dropout(p=dropout_rate),
-#             nn.Linear(in_features, num_classes)
-#         )
+        in_features = model.classifier[1].in_features
+        model.classifier = nn.Sequential(
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(in_features, num_classes)
+        )
 
-#         # Garantir que o classifier seja treinável
-#         for param in model.classifier.parameters():
-#             param.requires_grad = True
+        # Garantir que o classifier seja treinável
+        for param in model.classifier.parameters():
+            param.requires_grad = True
 
-#     elif backbone == "efficientnet_b0":
-#         model = models.efficientnet_b0(weights=pretrained)
+    elif backbone == "efficientnet_b0":
+        model = models.efficientnet_b0(weights=pretrained)
 
-#         # Congela tudo
-#         for param in model.parameters():
-#             param.requires_grad = False
+        # Congela tudo
+        for param in model.parameters():
+            param.requires_grad = False
 
-#         # Descongela os últimos blocos
-#         for param in model.features[-trainable_blocks:].parameters():
-#             param.requires_grad = True
+        # Descongela os últimos blocos
+        for param in model.features[-trainable_blocks:].parameters():
+            param.requires_grad = True
 
-#         in_features = model.classifier[1].in_features
-#         model.classifier = nn.Sequential(
-#             nn.Dropout(p=dropout_rate),
-#             nn.Linear(in_features, num_classes)
-#         )
+        in_features = model.classifier[1].in_features
+        model.classifier = nn.Sequential(
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(in_features, num_classes)
+        )
 
-#         # Garantir que o classifier seja treinável
-#         for param in model.classifier.parameters():
-#             param.requires_grad = True
+        # Garantir que o classifier seja treinável
+        for param in model.classifier.parameters():
+            param.requires_grad = True
 
-#     else:
-#         raise ValueError("backbone deve ser 'mobilenet' ou 'efficientnet_b0'")
+    else:
+        raise ValueError("backbone deve ser 'mobilenet' ou 'efficientnet_b0'")
 
-#     return model
+    return model
 
 # saida1
 # def criar_modelo(backbone: str, num_classes: int, pretrained='DEFAULT'):
@@ -166,69 +166,69 @@ from .utils import *
 
 #     return model
 
-def criar_modelo(
-    backbone: str,
-    num_classes: int,
-    dataset_type: str, # <--- NOVO PARÂMETRO AQUI
-    pretrained='DEFAULT',
-    dropout_rate=0.2,
-    trainable_blocks=3
-):
-    backbone = backbone.lower()
+# def criar_modelo(
+#     backbone: str,
+#     num_classes: int,
+#     dataset_type: str, # <--- NOVO PARÂMETRO AQUI
+#     pretrained='DEFAULT',
+#     dropout_rate=0.2,
+#     trainable_blocks=3
+# ):
+#     backbone = backbone.lower()
     
-    # Verifica se estamos lidando com imagens originais
-    is_originais = (dataset_type == "originais")
+#     # Verifica se estamos lidando com imagens originais
+#     is_originais = (dataset_type == "originais")
 
-    if backbone == "mobilenet":
-        model = models.mobilenet_v2(weights=pretrained)
+#     if backbone == "mobilenet":
+#         model = models.mobilenet_v2(weights=pretrained)
 
-        # Só congela se for o dataset original
-        if is_originais:
-            for param in model.parameters():
-                param.requires_grad = False
-            # Descongela os últimos blocos
-            for param in model.features[-trainable_blocks:].parameters():
-                param.requires_grad = True
+#         # Só congela se for o dataset original
+#         if is_originais:
+#             for param in model.parameters():
+#                 param.requires_grad = False
+#             # Descongela os últimos blocos
+#             for param in model.features[-trainable_blocks:].parameters():
+#                 param.requires_grad = True
 
-        in_features = model.classifier[1].in_features
-        model.classifier = nn.Sequential(
-            nn.Dropout(p=dropout_rate),
-            nn.Linear(in_features, num_classes)
-        )
+#         in_features = model.classifier[1].in_features
+#         model.classifier = nn.Sequential(
+#             nn.Dropout(p=dropout_rate),
+#             nn.Linear(in_features, num_classes)
+#         )
 
-        # Garantir que o classifier seja treinável
-        for param in model.classifier.parameters():
-            param.requires_grad = True
+#         # Garantir que o classifier seja treinável
+#         for param in model.classifier.parameters():
+#             param.requires_grad = True
 
-    elif backbone == "efficientnet_b0":
-        model = models.efficientnet_b0(weights=pretrained)
+#     elif backbone == "efficientnet_b0":
+#         model = models.efficientnet_b0(weights=pretrained)
 
-        # Só congela se for o dataset original
-        if is_originais:
-            for param in model.parameters():
-                param.requires_grad = False
-            # Descongela os últimos blocos
-            for param in model.features[-trainable_blocks:].parameters():
-                param.requires_grad = True
+#         # Só congela se for o dataset original
+#         if is_originais:
+#             for param in model.parameters():
+#                 param.requires_grad = False
+#             # Descongela os últimos blocos
+#             for param in model.features[-trainable_blocks:].parameters():
+#                 param.requires_grad = True
 
-        in_features = model.classifier[1].in_features
-        model.classifier = nn.Sequential(
-            nn.Dropout(p=dropout_rate),
-            nn.Linear(in_features, num_classes)
-        )
+#         in_features = model.classifier[1].in_features
+#         model.classifier = nn.Sequential(
+#             nn.Dropout(p=dropout_rate),
+#             nn.Linear(in_features, num_classes)
+#         )
 
-        for param in model.classifier.parameters():
-            param.requires_grad = True
+#         for param in model.classifier.parameters():
+#             param.requires_grad = True
 
-    else:
-        raise ValueError("backbone deve ser 'mobilenet' ou 'efficientnet_b0'")
+#     else:
+#         raise ValueError("backbone deve ser 'mobilenet' ou 'efficientnet_b0'")
 
-    return model
+#     return model
 
 def carregar_modelo(backbone, num_classes, dataset_type, path_weights):
     print(f"Carregando {backbone} de {path_weights}...")
 
-    model = criar_modelo(backbone, num_classes, dataset_type, pretrained=None) # Cria o modelo com a arquitetura correta
+    model = criar_modelo(backbone, num_classes, pretrained=None) # Cria o modelo com a arquitetura correta
 
     # Carrega os pesos treinados
     try:
