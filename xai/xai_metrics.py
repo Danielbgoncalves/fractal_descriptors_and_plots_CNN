dd9a91083b60d84163b5f264abf4dce9906275c4
Daniel Borges <44danbor@gmail.com>
2026-08-03 09:10:48 -0300

    fix: update argument name in calcula_matricas_xai for clarity

--- xai/xai_metrics.py
+++ xai/xai_metrics.py
@@ -71,7 +71,7 @@ def calcula_matricas_xai(
         return_aggregate=False,
         display_progressbar=False,
         disable_warnings=True,
-    )(model=modelo, x_batch=x_batch, y_batch=y_batch, a_batch=a_batch,
+    )(model=modelo, x_batch=x_batch, y_batch=y_batch, explain_func=explain_func,
       channel_first=True, device=device)
 
     complexity = quantus.Sparseness(
